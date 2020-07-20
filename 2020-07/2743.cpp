4ec3d74165900c7b64cd5ad9eb8ff55e206af01e
tjrdud123 <youmr100@naver.com>
2020-07-20 23:50:47 +0900

    200720

---
+++ 2020-07/2743.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	string str;
+	cin >> str;
+	cout << str.size();
+	return 0;
+}
