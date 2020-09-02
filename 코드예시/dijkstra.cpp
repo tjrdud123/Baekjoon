3feedfb4431babdd6825dc3c0dc84acc0eeb1396
tjrdud123 <youmr100@naver.com>
2020-09-02 16:34:05 +0900

    다익스트라 ex코드

---
+++ 코드예시/dijkstra.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <cmath>
+#include <queue>
+#include <ctype.h>
+using namespace std;
+vector<vector<pair<int, int> > > adj;
+int nVertices;
+vector<int> dijkstra(int src) {
+	int INF = 2000000000;
+	vector<int> dist(nVertices, INF);
+	dist[src] = 0;
+
+	priority_queue<pair<int, int> > pq;
+	pq.push(make_pair(0, src));
+
+	while (!pq.empty()) {
+		int cost = -pq.top().first;
+		int here = pq.top().second;
+		pq.pop();
+
+		if (dist[here] < cost) continue;
+
+		for (int i = 0; i < adj[here].size(); i++) {
+			int there = adj[here][i].first;
+			int nextDist = cost + adj[here][i].second;
+
+			if (dist[there] > nextDist) {
+				dist[there] = nextDist;
+				pq.push(make_pair(-nextDist, there));
+			}
+		}
+	}
+	return dist;
+}
+int main(void) {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	return 0;
+}
