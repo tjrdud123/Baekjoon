1ee4cf1c3ebd9218bfa59548bcab1d2c90534e0c
tjrdud123 <youmr100@naver.com>
2020-10-07 00:57:39 +0900

    add gitignore

--- 2020-10/skeleton.cpp
+++ 2020-10/skeleton.cpp
@@ -2,9 +2,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+int N, M;
+vector<int> A;
 int main(void) {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
+    A.resize(N + 1);
     
+    for(int i = 0; i <= N; i++) A[i] = i;
+    for(int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        int t = A[a];
+        A[a] = A[b];
+    }
+    for(int i = 1; i <= N; i++) {
+        cout << A[i] << " ";
+    }
     return 0;
 }
