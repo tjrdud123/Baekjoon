4de95e57a713b47c52dc886af5b4776bec81a025
tjrdud123 <youmr100@naver.com>
2020-10-07 00:40:03 +0900

    201007

---
+++ 2020-10/10813.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+int N, M;
+vector<int> A;
+int main(void) {
+    cin >> N >> M;
+    A.resize(N + 1);
+    
+    for(int i = 0; i <= N; i++) A[i] = i;
+    for(int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        int t = A[a];
+        A[a] = A[b];
+        A[b] = t;
+    }
+    for(int i = 1; i <= N; i++) {
+        cout << A[i] << " ";
+    }
+    return 0;
+}
---
+++ 2020-10/skeleton.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    return 0;
+}
